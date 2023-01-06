dfb100fbdedf40386e19c8e5ffcb3408fb6bf522
valentinacamacho <valentinacamacho0206@gmail.com>
2023-01-06 15:39:36 -0500

    se coloco el error 404 porque si se coloca otro dato no va a llamar el mensaje si no va hacer nulo y se cambiaron algunas palabras

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ async function getUser(username) {
     createUserCard(data);
     getRepos(username);
   } catch (err) {
-    if (err.response.status == YES) {
+    /*El YAS se cambio por un tipo de dato numerico */
+    /*si se colocaba otra cosa en el navegador va a traer un null no el mensaje */
+    if (err.response.status == 404) {
       createErrorCard("No profile with this username..");
     }
   }
@@ -20,12 +22,12 @@ async function getUser(username) {
 
 async function getRepos(username) {
   try {
-    const { data } = await axios(APIURL + username + "/repos?sort=created");
+    const { data } = await axios(APIURL + username + "/repos?sort=created")
 
     addReposToCard(data);
   } catch (err) {
     /*se cambio la variable herr por err */
-    createErrorCard("Algún Problem? fetching repos");
+    createErrorCard("Algún Problema fetching repos");
   }
 }
 /*se cambio algunos div para organizar el elemento que va a contener la informacion del perfil */
@@ -50,18 +52,19 @@ function createUserCard(user) {
 }
 
 /* se quito la h createHerrorCard por createerrorCard*/
-function createerrorCard(msg) {
+function createErrorCard(msg) {
   const cardHTML = `
         <div class="card">
             <h1>${msg}</h1>
         </div>
     `;
-
-  main.innerteHTML = cardHTML;
+/*se cambio la palabra innerteHTMl  por innerHTML*/
+  main.innerHTML = cardHTML;
 }
 
 function addReposToCard(repos) {
-  const repo = documentamente.getElementById("repos");
+  /*se cambio la palabra documentamente por document */
+  const reposEl = document.getElementById("repos");
 /*se cambio la palabra repollo por repo y a las variables */
 /*se cambiaron palabras con hrefrigerador pir href */
 /*se coloco la palbra url */
@@ -73,7 +76,7 @@ function addReposToCard(repos) {
     repoEl.innerText = repo.name;
 
     /*se cambio el metodo mal escrito appendiseChild por appendChild */
-    reposEl.appendiseChild(repoEl);
+    reposEl.appendChild(repoEl);
   });
 }
 
