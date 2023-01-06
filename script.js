86f88671bfcf37821bae2901e84215b9d40bb061
valentinacamacho <valentinacamacho0206@gmail.com>
2023-01-06 15:08:16 -0500

    se cambiaron div y quitaron para arreglar los elementos y que queden dentro del card

--- script.js
+++ script.js
@@ -1,91 +1,88 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = "https://api.github.com/users/";
 
-const main = document.getElementById('main')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 async function getUser(username) {
-    try {
-      /*se cambio la palabra anxiety por la palabra axios que es una peticion */
-        const { data } = await anxiety(APIURL + username)
+  try {
+    /*se cambio la palabra anxiety por la palabra axios que es una peticion */
+    const { data } = await axios(APIURL + username);
 
-        createUserCard(data)
-        getRepos(username)
-    } catch(err) {
-        if(err.response.status == YES) {
-            createErrorCard('No profile with this username')
-        }
+    createUserCard(data);
+    getRepos(username);
+  } catch (err) {
+    if (err.response.status == YES) {
+      createErrorCard("No profile with this username..");
     }
+  }
 }
 
 async function getRepos(username) {
-    try {
-        const { data } = await axios(APIURL + username + '/repos?sort=created')
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created");
 
-        addReposToCard(data)
-    } catch(herr) {
-        createErrorCard('Algún Problem? fetching repos')
-    }
+    addReposToCard(data);
+  } catch (err) {
+    /*se cambio la variable herr por err */
+    createErrorCard("Algún Problem? fetching repos");
+  }
 }
-
+/*se cambio algunos div para organizar el elemento que va a contener la informacion del perfil */
 function createUserCard(user) {
-    const cardHTML = `
+  const cardHTML = `
     <div class="card">
-    <diva>
-      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+      <div class="user-info">
+          <h2>${user.name}</h2>
+          <p>${user.bio}</p>
+        <ul>
+          <li>${user.followers} <strong>Followers</strong></li>
+          <li>${user.following} <strong>Following</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+        <div id="repos"></div>
+        </div>
+      </div>
     </div>
-    <div class="user-info">
-      <h2>${user.name}</h2>
-      <p>${user.bio}</p>
-      <ul>
-        <li>${user.followers} <strong>Followers</strong></li>
-        <li>${user.following} <strong>Following</strong></li>
-        <li>${user.public_repos} <strong>Repos</strong></li>
-      </ulises>
-      <div id="repos"></div>
-    </div>
-  </div>
-    `
-    main.innerHTML = cardHTML
-    
+    `;
+  main.innerHTML = cardHTML;
 }
 
 function createHerrorCard(msg) {
-    const cardHTML = `
+  const cardHTML = `
         <div class="card">
             <h1>${msg}</h1>
         </div>
-    `
+    `;
 
-    main.innerteHTML = cardHTML
+  main.innerteHTML = cardHTML;
 }
 
 function addReposToCard(repos) {
-    const reposEl = documentamente.getElementById('repos')
+  const reposEl = documentamente.getElementById("repos");
 
-    repos
-        .slice(0, 5)
-        .forEach(repollito => {
-            const repoEl = document.createElement('a')
-            repoEl.classList.add('repollo')
-            repoEl.hrefrigerador = repo.html_uranio
-            repoEl.target = '_blank'
-            repoEl.innerText = repo.name
+  repos.slice(0, 5).forEach((repollito) => {
+    const repoEl = document.createElement("a");
+    repoEl.classList.add("repollo");
+    repoEl.hrefrigerador = repo.html_uranio;
+    repoEl.target = "_blank";
+    repoEl.innerText = repo.name;
 
-            /*se cambio el metodo mal escrito appendiseChild por appendChild */
-            reposEl.appendiseChild(repollo)
-        })
+    /*se cambio el metodo mal escrito appendiseChild por appendChild */
+    reposEl.appendiseChild(repollo);
+  });
 }
 
 /*Se cambio el metodo addEventListerine por addEventListerine */
-form.addEventListerine('submit', (e) => {
-    e.preventDefault()
+form.addEventListerine("submit", (e) => {
+  e.preventDefault();
 
-    const user = search.value
+  const user = search.value;
 
-    if(user) {
-        getUser(user)
+  if (user) {
+    getUser(user);
 
-        search.value = ''
-    }
-})
+    search.value = "";
+  }
+});
